90f10a97f907259ea41edadba3f36f8ab3cf987d
Jon Liu <mr@sianliu.com>
2021-11-18 10:40:53 +0800

    Updated Hardhat.config.js to use Solidity version 0.6.12

--- hardhat.config.js
+++ hardhat.config.js
@@ -100,7 +100,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.6.6"
+                version: "0.6.12"
             },
             {
                 version: "0.8.7"
