86759426e6356398667b61e2647db9231e1aed7f
Alexandre Melo <amelo@harlemnext.com>
2021-11-18 16:36:13 -0300

    Warrior NFT contract

--- contracts/tws/TheWarriorSpectrumWarriorsV2.sol
+++ contracts/tws/TheWarriorSpectrumWarriorsV2.sol
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-/*
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
@@ -13,9 +10,61 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 // VRF Coordinator	0x3d2341ADb2D31f1c5530cDC622016af293177AE0
 // Key Hash	        0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da
 // Fee              0.0001 LINK
-/*
-contract TheWarriorSpectrumWarriorsV2 is ERC721Enumerable, VRFConsumerBase,  Ownable, ChainlinkClient  {
-    using Counters for Counters.Counter;
-    using Chainlink for Chainlink.Request;
 
-}*/
+contract TheWarriorSpectrumWarriorsV2 is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+    using Strings for uint256;
+
+    mapping(uint256 => string) private _tokenURIs;
+
+    string private _baseURIextended;
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 public constant MAX_SUPPLY = 10000;
+    uint256 public constant PRICE_PER_TOKEN = 0.001 ether;
+
+    constructor() ERC721("TheWarriorSpectrumWarriors", "TWS") {}
+
+
+    function mint(address _to, string memory metadataURI) public onlyOwner {
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _mint(_to, tokenId);
+        _setTokenURI(tokenId, metadataURI);
+    }
+
+    function setBaseURI(string memory baseURI_) external onlyOwner() {
+        _baseURIextended = baseURI_;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIextended;
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return _tokenURIs[tokenId];
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+}
