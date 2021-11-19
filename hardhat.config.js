4587c524885396850b9b099860b70bddcf996c82
Jon Liu <mr@sianliu.com>
2021-11-19 17:29:24 +0800

    Initial commit Chainlink VRF functionality, using Solidity version 0.7.6, and installed open-zeppelin contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -100,6 +100,9 @@ module.exports = {
     solidity: {
         compilers: [
             {
+                version: "0.7.6"
+            },
+            {
                 version: "0.8.7"
             },
             {
